cfbdf24f6f8f8c8f958bc04fc2d0ad3d03cb6c7b
Sara Amiry <saraamiry24@gmail.com>
2020-06-07 08:47:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(num)
+  i = 0 
+  #new_array = []
+ while i < num.length 
+ # new_array << 
+  yield (num[i])
+  i += 1
+ end
 end
+ 
+
+
+ 
